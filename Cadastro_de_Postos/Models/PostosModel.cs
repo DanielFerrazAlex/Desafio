986298d94b00693e3791a0329f5c48020df9b336
Daniel Ferraz <danielferrazalex@hotmail.comgit config --global user.email danielferrazalex@hotmail.comgit config --global user.email danielferrazalex@hotmail.com>
2024-06-25 23:12:51 -0300

    implementando Variaveis de Ambiente para conexão com Banco e Models

---
+++ Cadastro_de_Postos/Models/PostosModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Cadastro_de_Postos.Models
+{
+    public class PostosModel
+    {
+        public string? Nome_Posto { get; set; }
+        public VacinasModel? Vacinas { get; set; }
+    }
+}
---
+++ Cadastro_de_Postos/Models/VacinasModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cadastro_de_Postos.Models
+{
+    public class VacinasModel
+    {
+        public int Lote { get; set; }
+        public string? Nome_Vacina { get; set; }
+        public string? Fabricante { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime Data_Validade { get; set; }
+    }
+}
